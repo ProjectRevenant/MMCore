51921a4d0c770e2114512e23b381e21207a8739b
Flo0 <flo.roma@web.de>
2022-08-23 18:18:07 +0200

    Some stuff

--- src/main/java/com/gestankbratwurst/core/mmcore/util/common/UtilChunk.java
+++ src/main/java/com/gestankbratwurst/core/mmcore/util/common/UtilChunk.java
@@ -11,6 +11,7 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /*******************************************************
  * Copyright (C) Gestankbratwurst suotokka@gmail.com
